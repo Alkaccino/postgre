2fa05b2d41c9193ce818a58e51027390e87284b4
Alkaccino <alkan2506@outlook.com>
2026-09-25 12:41:47 +0200

    connect with db and creating table

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"strings"
+
+	"github.com/jackc/pgx/v5"
 )
 
 type Item struct {
@@ -14,6 +20,37 @@ type Item struct {
 }
 
 func main() {
+	env, err := os.ReadFile(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	envData := strings.Split(string(env), "\n")
+
+	_, db_user, _ := strings.Cut(envData[0], "=")
+	_, db_password, _ := strings.Cut(envData[1], "=")
+	_, db_host, _ := strings.Cut(envData[2], "=")
+	_, db_port, _ := strings.Cut(envData[3], "=")
+	_, db_name, _ := strings.Cut(envData[4], "=")
+
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", db_user, db_password, db_host, db_port, db_name)
+
+	conn, err := pgx.Connect(context.Background(), connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close(context.Background())
+
+	create_sql, err := os.ReadFile("./sql/create_table.sql")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = conn.Exec(context.Background(), string(create_sql))
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	http.HandleFunc("GET /", handler)
 	http.ListenAndServe(":8080", nil)
 }
@@ -30,5 +67,3 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 }
-
-// See, here is another commit
